9f0731212b016a286f519b6c5007a6d1f3b900bc
NIHAL <ku2407u892@karnavatiuniversity.edu.in>
2026-09-11 10:13:15 +0530

    Use PostgreSQL instead of SQLite

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
 
 # Replace these with your actual Render Postgres DB URL
-DATABASE_URL = "postgresql://ai_railops_db_user:tHCuLObd3lAGhhJW1eLrs5hOZfSPEN4s@dpg-dahnuu3m8hqs73cl4b6g-a/ai_railops_db"
+DATABASE_URL = "postgresql://ai_railops_db_user:tHCuLObd3lAGhhJW1eLrs5hOZfSPEN4s@dpg-dahnuu3m8hqs73cl4b6g-a.oregon-postgres.render.com/ai_railops_db"
 
 # If using Render, it will give you this URL automatically
 # Example: postgresql://user:pass@db.render.com:5432/mydb
